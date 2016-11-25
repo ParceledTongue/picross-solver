e9368ac473cf9ca8df8e74960ea89d3d2b2ee202
ParceledTongue <zach@palumbo.io>
2016-11-25 04:45:04 -0500

    Rename "proto" to "solver"
    Signed-off-by: ParceledTongue <zach@palumbo.io>

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
-import proto as p
+import solver as s
 import examples as e
 
 import timeit
 
-p.solve(e.duck)
-p.solve(e.swan)
-p.settings['print_steps'] = True
-p.settings['print_possibility_calcs'] = True
-p.solve(e.snowflake)
+s.solve(e.duck)
+s.solve(e.swan)
+s.settings['print_steps'] = True
+s.settings['print_possibility_calcs'] = True
+s.solve(e.snowflake)
